96a00ee4cb4ce6cdc0b0ff52676762ca131d815e
Lars Helge Scheel <larshelge.scheel@gmail.com>
2019-11-02 16:53:02 +0100

    Soon

--- core.cpp
+++ core.cpp
@@ -9,71 +9,131 @@
 using V2 = Eigen::Vector2d;
 using V3 = Eigen::Vector3d;
 using V4 = Eigen::Vector4d;
+using Q = Eigen::Quaternion<double>;
 
-struct Data {
+struct Barycentric {
 
-  Data(size_t resolution, size_t num_frames) : resolution(resolution), num_frames(num_frames) {
-
-    scales_s.resize(resolution);
-    locations_s.resize(resolution);
-    rotations_s.resize(resolution);
-    for (size_t r = 0; r < resolution; r++) {
-      double rd = static_cast<double>(r)/static_cast<double>(resolution);
-      double ra = rd * PI * 2.0;
-      auto & scales = scales_s[r];
-      auto & locations = locations_s[r];
-      auto & rotations = rotations_s[r];
-
-
-      scales.resize(num_frames);
-      locations.resize(num_frames);
-      rotations.resize(num_frames);
-      for (size_t f = 0; f < num_frames; f++) {
-        double fd = static_cast<double>(f)/static_cast<double>(num_frames);
-        double fa = fd * PI * 2.0;
-        auto & scale = scales[f];
-        auto & location = locations[f];
-        auto & rotation = rotations[f];
-
-        scale = V3::Ones();
-
-        if (r > 0) {
-          location = Eigen::Quaternion<double>(
-              rotations_s[r-1][f][0],
-              rotations_s[r-1][f][1],
-              rotations_s[r-1][f][2],
-              rotations_s[r-1][f][3]
-          ) * V3::UnitX();
-          location += locations_s[r-1][f];
-        } else {
-          location = V3::Zero();
-        }
-
-        V2 rotA(sin(rd*5.0), cos(rd*5.0));
-        V2 rotB(-sin(fd+PI), cos(fd+PI));
-
-        rotA *= sin(fd*rd*rd*50.0);
-        rotB *= cos(fd*rd*rd*50.0);
-
-        rotation = V4(rotA.x(), rotA.y(), rotB.x(), rotB.y());
-      }
-    }
+  V3 get_point(const Q & q) {
+    return q.w() * a + q.x() * b + q.y() * c + q.z() * d;
   }
 
-  size_t resolution;
-  size_t num_frames;
-
-  std::vector<std::vector<Eigen::Vector3d>> scales_s;
-  std::vector<std::vector<Eigen::Vector3d>> locations_s;
-  std::vector<std::vector<Eigen::Vector4d>> rotations_s;
+  V3 a = V3(1,1,1);
+  V3 b = V3(-1,-1,1);
+  V3 c = V3(-1,1,-1);
+  V3 d = V3(1,-1,-1);
 
 };
 
-extern "C" {
-  Data* construct(size_t resolution, size_t num_frames) {
-    return new Data(resolution, num_frames);
+struct QuaternionFourier {
+
+  QuaternionFourier(size_t max_spin = 5) {
+
+    num_terms = (max_spin*2 + 1) * (max_spin*2 + 1);
+
+    u.reserve(num_terms);
+    u.push_back(V2::Zero());
+    for (int spin_u = 1; spin_u < max_spin + 1; spin_u ++) {
+      for (int spin_v = 1; spin_v < max_spin + 1; spin_v ++) {
+        u.push_back(V2(-spin_u, -spin_v));
+        u.push_back(V2(-spin_u,  spin_v));
+        u.push_back(V2( spin_u, -spin_v));
+        u.push_back(V2( spin_u,  spin_v));
+      }
+    }
+
+    f_hat.resize(num_terms, Q(1.0,0.0,0.0,0.0));
+
   }
-  double * get_scales(Data* data, size_t i) { return data->scales_s[i][0].data(); }
-  double * get_locations(Data* data, size_t i) { return data->locations_s[i][0].data(); }
-  double * get_rotations(Data* data, size_t i) { return data->rotations_s[i][0].data(); }
+
+  Q get_term(size_t n, V2 x) {
+
+    V2 phase = 2.0 * PI * x.cwiseProduct(u[n]);
+
+    Q left(cos(phase[0]),
+           sin(phase[0]),
+           0.0,
+           0.0);
+
+    Q right(cos(phase[1]),
+            0.0,
+            sin(phase[1]),
+            0.0);
+
+    return left * f_hat[n] * right;
+
+  }
+
+  size_t num_terms;
+
+  std::vector<V2> u;
+  std::vector<Q> f_hat;
+
+};
+
+struct BlenderData {
+
+  BlenderData(size_t num_objects, size_t num_frames) : num_objects(num_objects), num_frames(num_frames) {
+
+    scales_s.resize(num_objects);
+    locations_s.resize(num_objects);
+    rotations_s.resize(num_objects);
+
+    Barycentric bc;
+    QuaternionFourier qf(sqrt(num_objects)/2 + 1);
+
+    for (size_t o = 0; o < num_objects; o++) {
+      scales_s[o].resize(num_frames);
+      locations_s[o].resize(num_frames);
+      rotations_s[o].resize(num_frames);
+    }
+
+    for (size_t f = 0; f < num_frames; f++) {
+
+      V3 offset = V3::Zero();
+      double fd = static_cast<double>(f) / static_cast<double>(num_frames);
+      V2 x(sin(fd), cos(fd));
+
+      for (size_t o = 0; o < num_objects; o++) {
+
+        locations_s[o][f] = offset;
+
+        Q q_term = qf.get_term(o, x);
+
+        V3 add_offset = bc.get_point(q_term);
+
+        scales_s[o][f][0] = add_offset.norm();
+        scales_s[o][f][1] = 1.0;
+        scales_s[o][f][2] = 1.0;
+
+        Q to_offset = Q::FromTwoVectors(V3::UnitX(), add_offset);
+
+        rotations_s[o][f][0] = to_offset.w();
+        rotations_s[o][f][1] = to_offset.x();
+        rotations_s[o][f][2] = to_offset.y();
+        rotations_s[o][f][3] = to_offset.z();
+
+        offset += add_offset;
+
+      }
+    }
+
+  }
+
+  size_t num_objects;
+  size_t num_frames;
+
+  std::vector<std::vector<V3>> scales_s;
+  std::vector<std::vector<V3>> locations_s;
+  std::vector<std::vector<V4>> rotations_s;
+
+};
+
+
+extern "C" {
+  BlenderData* construct(size_t resolution, size_t num_frames) {
+    return new BlenderData(resolution, num_frames);
+  }
+  double * get_scales(BlenderData* data, size_t i) { return data->scales_s[i][0].data(); }
+  double * get_locations(BlenderData* data, size_t i) { return data->locations_s[i][0].data(); }
+  double * get_rotations(BlenderData* data, size_t i) { return data->rotations_s[i][0].data(); }
 }
